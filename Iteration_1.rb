74ebc7709a162a470ff38f4036423b5cab846a95
aieest <aie.estidola@gmail.com>
2023-09-14 21:23:24 +0800

    Iteration 1: Iterate through an Array

---
+++ Iteration_1.rb
@@ -0,0 +1,10 @@
+# The given array has a series of numbers.
+# Your task is print each number
+
+numbers = [0, 1, 5, 7, 18]
+
+#Write your cocde below this line
+
+numbers.each do |number|
+    puts number
+  end
